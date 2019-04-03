cc15e1215e7e6288fdb6a52c885960fab0182302
wj5019 <45660555+wj5019@users.noreply.github.com>
2019-04-03 23:22:11 +0800

    数据库连接

---
+++ JDBC.java
@@ -0,0 +1,28 @@
+package cn.tedu.javaweb.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//专门用于访问数据库，打开和关闭数据库的连接
+public class JDBC {
+//自定义方法：打开数据库的连接
+	public static Connection getConnection() {
+//定义一个数据库连接
+		Connection conn=null;
+		//加载mysql的驱动包
+		try {
+			Class.forName("com.mysql.jdbc.Diver");
+			String url="jdbc:mysql://localhost:3306/mysql";
+			conn=DriverManager.getConnection(url,"root","12345678");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//返回数据库连接
+		return conn;
+	}
+}
